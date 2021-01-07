806272a5d2bb84e70cbf8f775da1963098b5caba
Pjqa <>
2021-01-07 17:52:09 +0000

    added files

---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,14 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+	 
+	
+
+	// 3
+		for (int all = 1; all <= 10; all++) {
+			System.out.println(" 1 2 3 4 5 6 7 8 9 10");
+}
+		}
+}
